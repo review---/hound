c37239db2f5feb890074c685a7d9ca2f96e627c3
Adarsh Pandit <adarsh@thoughtbot.com>
2014-12-19 17:13:53 -0800

    add rake task to update repo information

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140926163029) do
+ActiveRecord::Schema.define(version: 20141219224840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_stat_statements"
 
   create_table "builds", force: true do |t|
     t.text     "violations"
